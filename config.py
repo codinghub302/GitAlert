ee93edac23a89db004a3bdd76a7ab7ba52f77444
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <128347344+codinghub302@users.noreply.github.com>
2023-03-26 14:48:13 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Your Bot token in the quotes from @BotFather
+BOT_TOKEN = ""
+
+# Project Name of your Project
+PROJECT_NAME = "GitAlert"
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/codinghub302/GitAlert"
+
+# For /connect
+HEROKU_APPNAME = "gitalertbot"
